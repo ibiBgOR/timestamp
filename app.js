3cc2b9dd77a43a564089da3ff893094383a7a36c
pascalhelbig <pascalhelbig@gmail.com>
2016-02-01 10:16:02 +0100

    parse and format dates with moment.js

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/:date', function (req, res) {
+  var momentDate = moment(req.params.date, ['MMMM D, YYYY', 'X'], true);
+
+  if (momentDate.isValid()) {
+    res.json({
+      unix: momentDate.unix(),
+      natural: momentDate.format('MMMM D, YYYY')
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+});
+
 app.listen(8080, function () {
   console.log('Server running on 8080');
 });
+
